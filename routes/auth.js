55d754fa120a9923825175b7cfe191f70383f0f2
PawanAK <pawanajjark@gmail.com>
2024-01-28 06:35:48 +0530

    v6

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
+const UserController = require('../controllers/UserController');
 
-router.post('/signup', authController.signup);
-router.post('/signin', authController.signin);
-router.get('/me', authController.getMe);
+// POST /v1/auth/signup
+router.post('/signup', UserController.signup);
+
+// POST /v1/auth/signin
+router.post('/signin', UserController.signin);
+
+// GET /v1/auth/me
+router.get('/me', authMiddleware, UserController.getMe);
 
 module.exports = router;
